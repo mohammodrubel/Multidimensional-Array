484dd322b42f559ec32eaa728eaa5d3a4dddf6ab
Mohammodrubel <74967048+mohammodrubel@users.noreply.github.com>
2021-08-25 19:09:55 +0600

    Add files via upload

---
+++ New Text Document.js
@@ -0,0 +1,17 @@
+const bangladesh = ['Dhaka','ctg','sylt']
+const india = ['kolkata','chennai','mumbai']
+const chine =['uhan','baizing','honkkong']
+const mainArray = [bangladesh,india,chine]
+const needIndea = mainArray[1]
+const needBangladesh = mainArray[0]
+const needChine = mainArray[2]
+console.log(needBangladesh)
+
+const myArray = [
+    ['ashik',20,35,67,43,24],
+    ['rabbi',34,56,78,90,32],
+    ['tofa',34,34,23,45,56],
+    ['hasem',45,32,34,56,32],
+]
+console.log(myArray[1][4])
+console.log(myArray[1])
